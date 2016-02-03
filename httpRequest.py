70488e013535db64e8cae6e2e40ad36b6e238adb
weixing <weixing@tudou.com>
2016-02-03 14:28:11 +0800

    请求http接口，并解析返回的json数据

---
+++ httpRequest.py
@@ -0,0 +1,50 @@
+#-*- coding: UTF-8 -*- 
+import urllib
+import urllib2
+import json
+import time
+import traceback
+
+
+def requestSingle(id):
+    url = 'http://testurl'
+    t=0
+    flag=True
+    #初始化字典
+    _jsonResult={} 
+    _time=str(time.time())
+    while (t<3 and flag):
+        try:
+            msg='{"vid":'+id+',"status":1,"timestamp":'+_time+'}'
+            values = {'topic' : 'testtopic',
+                    'token' : 'testtoken',
+                    'msg':msg
+                    }
+            print msg
+
+            data = urllib.urlencode(values)
+            req = urllib2.Request(url, data)
+            response = urllib2.urlopen(req)
+            the_page = response.read()
+            _jsonResult=json.loads(the_page)
+            response.close()
+            flag=False
+        except Exception as e:
+            print e 
+            time.sleep(1)
+            t=t+1
+
+    return _jsonResult
+
+
+
+
+
+f=open('tvdvd.txt','rb')
+for line in f:
+    requestSingle(line)
+f.close()
+
+print "done!"
+
+
